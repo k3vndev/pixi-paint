e84e022245aeb7c624986c08e8b462cecc53245d
K3vnDev <kevinrpolo16@gmail.com>
2026-04-21 17:17:23 -0500

    Enhance FinishGameButton with tooltip and ref and update useTooltip documentation

--- src/components/play-page/FinishGameButton.tsx
+++ src/components/play-page/FinishGameButton.tsx
@@ -1,3 +1,5 @@
+import { useRef } from 'react'
+import { useTooltip } from '@/hooks/useTooltip'
 import { ColoredPixelatedImage } from '../images/ColoredPixelatedImage'
 import { Item } from '../paint-workspace/toolbar/Item'
 
@@ -6,10 +8,15 @@ interface Props {
   onClick?: () => void
 }
 
-export const FinishGameButton = ({ disabled, onClick }: Props) => (
-  <button className='not-lg:ml-1 relative' disabled={disabled} onClick={onClick}>
-    <Item className='size-full flex justify-center items-center not-lg:rounded-full not-lg:scale-90'>
-      <ColoredPixelatedImage icon='check' className='bg-theme-10 min-w-12 size-4/5 aspect-square' />
-    </Item>
-  </button>
-)
+export const FinishGameButton = ({ disabled, onClick }: Props) => {
+  const ref = useRef<HTMLButtonElement>(null)
+  useTooltip({ ref, text: "I've finished!", showWhen: !disabled })
+
+  return (
+    <button ref={ref} className='not-lg:ml-1 relative' disabled={disabled} onClick={onClick}>
+      <Item className='size-full flex justify-center items-center not-lg:rounded-full not-lg:scale-90'>
+        <ColoredPixelatedImage icon='check' className='bg-theme-10 min-w-12 size-4/5 aspect-square' />
+      </Item>
+    </button>
+  )
+}
--- src/hooks/useTooltip.ts
+++ src/hooks/useTooltip.ts
@@ -9,9 +9,16 @@ interface Params {
   ref: React.RefObject<HTMLElement | null>
   text: string
   showWhen?: boolean
-  waitTime?: number
 }
 
+/**
+ * A hook to show a tooltip when hovering or touching an element.
+ * It dispatches custom events to show/hide the tooltip, which can be listened to by a Tooltip component.
+ * @param params.ref The ref of the element to which the tooltip is attached.
+ * @param params.text The text to be shown in the tooltip.
+ * @param params.showWhen A boolean to control whether the tooltip should be shown or not. Default is true.
+ * @returns An object with a boolean `isShowingTooltip` indicating whether the tooltip is currently being shown.
+ */
 export const useTooltip = ({ ref: elementRef, text, showWhen = true }: Params) => {
   const [isBeingShown, setIsBeingShown] = useState(false)
   const refs = useFreshRefs({ showWhen, text })
